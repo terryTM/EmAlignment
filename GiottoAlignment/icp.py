a6ce2845f89a7ca75abc6a3da71322ee092a5a4c
Terry Ma <34947799+terryTM@users.noreply.github.com>
2023-05-17 14:48:58 -0400

    Add files via upload

--- GiottoAlignment/icp.py
+++ GiottoAlignment/icp.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.spatial import KDTree
+import matplotlib.pyplot as plt
+
 
 def icp(A, B, max_iterations=100, tolerance=1e-6):
     """
@@ -7,6 +9,9 @@ def icp(A, B, max_iterations=100, tolerance=1e-6):
     """
     # Initialize transformation matrix to identity matrix
     T = np.eye(3)
+    total_T = np.eye(3)
+    plt.figure()
+
     
     # Perform ICP for max_iterations
     for i in range(max_iterations):
@@ -22,14 +27,34 @@ def icp(A, B, max_iterations=100, tolerance=1e-6):
         # Calculate the covariance matrix of the points
         H = np.dot((A - centroid_A).T, closest_points - centroid_B)
         
+        # Use Singular Value Decomposition (SVD) to calculate rotation and translation matrices
+        U, S, Vt = np.linalg.svd(H)
+        R = np.dot(Vt.T, U.T)
+        t = centroid_B.T - np.dot(R, centroid_A.T)
+        
+        # Update transformation matrix
+        T_new = np.eye(3)
+        T_new[:2, :2] = R
+        T_new[:2, 2] = t
+        
         # Check for convergence
         if np.allclose(T, T_new, rtol=tolerance):
             break
         
         T = T_new
+
+
+        total_T = np.dot(total_T, T_new)
         
         # Apply transformation to points A
         A = np.hstack((A, np.ones((len(A), 1))))
         A = np.dot(T, A.T).T[:, :2]
+
+        plt.scatter(B[:, 0], -B[:, 1], c='b', label='B')
+        plt.scatter(A[:, 0], -A[:, 1], c='r', label='A')
+        if i == 0:  # only add legend in the first plot
+            plt.legend()
+        plt.title(f'Iteration {i+1}')
+        plt.show()
         
-    return T
+    return total_T, i
--- GiottoAlignment/preprocessing.py
+++ GiottoAlignment/preprocessing.py
@@ -45,50 +45,151 @@ import pandas as pd
 import math
 from icp import icp
 import matplotlib.pyplot as plt
+from sklearn.cluster import MiniBatchKMeans
+from scipy.spatial import KDTree
+from sklearn.cluster import DBSCAN
+
+
 #from simpleicp import PointCloud, SimpleICP
 
 
 
-
-img1 = cv2.imread("/Users/terryma/Documents/GiottoAlignment/emshot1.png", cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread("/Users/terryma/Documents/GiottoAlignment/emshot2.png", cv2.IMREAD_GRAYSCALE)
    
-def imageContour(path):   
+def preprocess(path):
+    img7 = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    img7 = cv2.threshold(img7, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+    img7[img7 < 250] = 0
+
+    img7 = cv2.GaussianBlur(img7, (5,5), 0)
+
+    img7 = cv2.medianBlur(img7,3)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
+    img7 = cv2.morphologyEx(img7, cv2.MORPH_BLACKHAT, kernel)
+
+    img7 = cv2.medianBlur(img7,15)
+
+
+    return img7
+
+
+def imageContour(img):   
     # Load the image as grayscale
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-
     img = cv2.equalizeHist(img)
-    img[img > 100] = 255
-    img = cv2.medianBlur(img, 15)
+    img[img > 50] = 255
 
-    img[img < 100] = 0
+    kernel = np.ones(7, np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+
+
+    #img[img < 100] = 0
 
     # Threshold the image to obtain a binary mask
     _, mask = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV)
 
+    mask = (255-mask)
+
+    return mask
+
+
+
     # Find the contours of all objects in the image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+
+
 
     # Find the contour with the largest area
     largest_contour = max(contours, key=cv2.contourArea)
 
+    cim = np.zeros_like(img)
+
+    cv2.drawContours(cim, largest_contour, -1, 255, 1)
+    cv2.imshow("con", cim)
+    cv2.waitKey(0)
     return largest_contour
 
 
-largest_contour1 = imageContour("/Users/terryma/Documents/GiottoAlignment/emshot1.png")
-largest_contour2 = imageContour("/Users/terryma/Documents/GiottoAlignment/emshot2.png")
+
+process_img1 = preprocess("/Users/terryma/Documents/GiottoAlignment/whole_embryo_H3K4me3_E11_50um.png")
+process_img2 = preprocess("/Users/terryma/Documents/GiottoAlignment/whole_embryo_H3K27me3_E11_50um.png")
+
+
+
+largest_contour1 = imageContour(process_img1)
+
+#cv2.imshow("con", largest_contour1)
+#cv2.waitKey(0)
+
+
+largest_contour2 = imageContour(process_img2)
 
 
 # Convert contour to point cloud
-def contourToPointcloud(contour):
-    # Reshape contour to (n_points, 1, 2)
-    points = contour.reshape(-1, 1, 2)
-    # Stack (x, y) coordinates vertically to create 2D array
-    pointcloud = np.vstack(points[:, 0])
+#def contourToPointcloud(contour):
+#    # Reshape contour to (n_points, 1, 2)
+#    points = contour.reshape(-1, 1, 2)
+#    # Stack (x, y) coordinates vertically to create 2D array
+#    pointcloud = np.vstack(points[:, 0])
+#    return pointcloud
+
+def image_to_pointcloud(img):
+    # Get the indices of the non-zero pixels
+    points = np.nonzero(img)
+    height = img.shape[0]
+    # Flip the y-coordinates
+    points_flipped = (points[1], height - points[0])
+    # Stack the indices to create the point cloud
+    pointcloud = np.column_stack(points_flipped)
     return pointcloud
 
-pointcloud1 = contourToPointcloud(largest_contour1)
-pointcloud2 = contourToPointcloud(largest_contour2)
+
+def downsample(pointcloud, n_clusters):
+    kmeans = MiniBatchKMeans(n_clusters=n_clusters, random_state=0, batch_size=6 * n_clusters)
+    kmeans = kmeans.partial_fit(pointcloud)
+    return kmeans.cluster_centers_
+
+
+
+def dbscan_outlier_removal(point_cloud, eps=80, min_samples=10):
+    """
+    Remove outliers from a point cloud using DBSCAN clustering.
+    
+    point_cloud: input point cloud as a numpy array.
+    eps: The maximum distance between two samples for one to be considered as in the neighborhood of the other.
+    min_samples: The number of samples in a neighborhood for a point to be considered as a core point.
+    """
+    # Apply DBSCAN on the point cloud.
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(point_cloud)
+
+    # Get the indices of the core samples.
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    # Create a mask to filter out the outliers.
+    mask = core_samples_mask
+
+    # Apply the mask to the point cloud.
+    filtered_point_cloud = point_cloud[mask]
+
+    return filtered_point_cloud
+
+
+
+pointcloud1 = image_to_pointcloud(largest_contour1)
+pointcloud2 = image_to_pointcloud(largest_contour2)
+
+pointcloud1 = downsample(pointcloud1, 2000)
+pointcloud2 = downsample(pointcloud2, 2000)
+
+pointcloud1 = dbscan_outlier_removal(pointcloud1)
+pointcloud2 = dbscan_outlier_removal(pointcloud2)
+
+pointcloud1[:,1] = pointcloud1[:,1] * -1
+pointcloud2[:,1] = pointcloud2[:,1] * -1
+
+
 print(pointcloud1.shape)
 print(pointcloud2.shape)
 A = pointcloud1
@@ -96,19 +197,22 @@ B = pointcloud2
 
 
 # Apply ICP to align A to B
-T = icp(A, B)
+T, i = icp(A, B)
+
+#| a  b  tx |
+#| c  d  ty |
+#| 0  0  1  |
+
 print(T)
 
-# Apply transformation to A and plot the aligned point clouds
-A_aligned = np.hstack((A, np.ones((len(A), 1))))
-A_aligned = np.dot(T, A_aligned.T).T[:, :2]
+# Apply transformation to points A
+A = np.hstack((A, np.ones((len(A), 1))))
+A = np.dot(T, A.T).T[:, :2]
 
-import matplotlib.pyplot as plt
-
-plt.scatter(B[:, 0], -B[:, 1], c='b', label='B')
+plt.scatter(B[:, 0], -B[:, 1], c='g', label='B')
 plt.scatter(A[:, 0], -A[:, 1], c='r', label='A')
-plt.scatter(A_aligned[:, 0], -A_aligned[:, 1], c='g', label='A aligned')
 plt.legend()
+plt.title(f'FINAL Iteration {i+1}')
 plt.show()
 
 
